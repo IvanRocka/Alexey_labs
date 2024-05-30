006a32892691e51106d58c876fd74ffbb739edee
IvanRocka <56740465+IvanRocka@users.noreply.github.com>
2024-05-30 17:02:28 +0300

    First commit
    12 лаба по ОАИП

---
+++ Lesson_12/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        // Переключение между вариантами рассчета 
+        private void Choose_mode_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Choose_mode.IsChecked == true)
+            {
+                Auto_evaluate_box.IsEnabled = true;
+                Self_evaluate_box.IsEnabled = false;
+            } else
+            {
+                Auto_evaluate_box.IsEnabled = false;
+                Self_evaluate_box.IsEnabled = true;
+            }
+        }
+    }
+}
